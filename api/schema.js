70e9d98f25316a30ebbc2e5a49d9fb878f572a64
rickychan0611 <ric0611@gmail.com>
2019-10-26 13:49:54 -0700

    added schema

--- api/schema.js
+++ api/schema.js
@@ -14,15 +14,36 @@ module.exports = gql`
   # scalar Date
 
   type Item {
-    _: Boolean
+    id: ID!
+    title: String!
+    imageurl: String
+    description: String
+    itemowner: User!
+    tags: [Tag]
+    created: Date!
+    borrower: User
   }
 
   type User {
-    _: Boolean
+    id: ID!
+    email: String!
+    fullname: String!
+    bio: String
+    items: [Item]
+    borrowed: [Item]
   }
 
   type Tag {
-    _: Boolean
+    id: ID!
+    title: String!
+  }
+
+  type File {
+    id: ID!
+    filename: String!
+    mimetype: String!
+    encoding: String!
+    itemid: ID!
   }
 
   type AuthPayload {
@@ -30,15 +51,18 @@ module.exports = gql`
   }
 
   input AssignedTag {
-    _: Boolean
+    id: ID!
+    title: String!
   }
 
   input AssignedBorrower {
-    _: Boolean
+    id: ID!
   }
 
   input NewItemInput {
-    _: Boolean
+    title: String!
+    description: String
+    tags: [AssignedTag]!
   }
 
   type Query {
@@ -49,6 +73,9 @@ module.exports = gql`
   }
 
   type Mutation {
-    addItem: Boolean
+    addItem(
+      item: NewItemInput!
+      image: Upload
+    ): Item
   }
 `;
